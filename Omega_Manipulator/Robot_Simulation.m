e48d5588e007eee2f85d457b70c84d022aabfcff
MahmoudTalaatRady <33983006+MahmoudTalaatRady@users.noreply.github.com>
2017-11-27 14:17:51 +0200

    Add files via upload

--- Omega_Manipulator/Robot_Simulation.m
+++ Omega_Manipulator/Robot_Simulation.m
@@ -9,8 +9,8 @@ L2 = Link([0, 0, li2, 0]);
 L3 = Link([0, 0, li3, 0]);
 L4 = Link([0, 0, li4, 0]);
 L = [L1 L2 L3 L4];
-Za3tar = SerialLink(L,'name','Za3tar');
+Manipulator = SerialLink(L,'name','Manipulator');
 
 XYZ_input = [150 160 150];
 theta = Online_ANFIS( XYZ_input );
-Za3tar.plot(theta);
+Manipulator.plot(theta);
